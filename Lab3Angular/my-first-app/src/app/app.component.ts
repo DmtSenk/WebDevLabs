a18a2c68b58668f7020f1e9aebf99edc2316cb2e
unknown <senikdima12@gmail.com>
2025-02-19 11:07:03 +0000

    'lab3'

---
+++ Lab3Angular/my-first-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,AComponent,BComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-first-app';
+  name: string = "Martin"
+  age:number = 25;
+}
+
